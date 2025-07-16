9d5d4cdc3f40d25450532ba42678542fcad0531b
sarazor <inf24172@lehre.dhbw-stuttgart.de>
2025-07-16 19:59:15 +0200

    Update Circle.cpp

--- src/Circle.cpp
+++ src/Circle.cpp
@@ -1,9 +1,11 @@
 #include "Circle.hpp"
 
+// Draws a filled circle at the current position with the specified size and color
 void Circle::draw(cv::Mat& frame) {
     cv::circle(frame, m_position, m_size.width / 2, m_color, -1);
 }
 
+// Checks if the center of the circle is inside the given rectangle (face)
 bool Circle::checkCollision(const cv::Rect& face) {
     int centerX = m_position.x + m_size.width / 2;
     int centerY = m_position.y + m_size.height / 2;
