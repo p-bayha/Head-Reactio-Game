00aea0e083e99dbfc5eb86b0b6d2a87e8d2adab5
sarazor <inf24172@lehre.dhbw-stuttgart.de>
2025-07-16 19:57:54 +0200

    Update GameObject.cpp

--- src/GameObject.cpp
+++ src/GameObject.cpp
@@ -1,13 +1,15 @@
 #ifndef GAMEOBJECT_HPP
 #define GAMEOBJECT_HPP
 
-#include "Utils.hpp" // Falls du Position, Size usw. brauchst
+#include "Utils.hpp" // Includes Position, Size, etc. if needed
 
+// Abstract base class for all game objects
 class GameObject {
 public:
     virtual void draw() = 0;
     virtual bool checkCollision(const GameObject& other) const = 0;
     virtual Position getPosition() const = 0;
+    // Virtual destructor for safe polymorphic deletion
     virtual ~GameObject() {}
 };
 
